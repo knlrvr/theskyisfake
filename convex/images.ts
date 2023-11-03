61c9065096502596b89382d6c5c64cdbcd3cbe8d
Kane <kanelariviere@outlook.com>
2023-11-02 23:17:39 -0400

    feat: img upload; style: img grid

---
+++ convex/images.ts
@@ -0,0 +1,51 @@
+import { v } from "convex/values";
+import { query } from "./_generated/server";
+
+export const getPosts = query({
+  args: {},
+  handler: async (ctx) => {
+    const posts = await ctx.db.query("posts").collect();
+    return Promise.all(
+      posts.map(async (post) => ({
+        ...post,
+        // If the message is an "image" its `body` is a `StorageId`
+        ...(post.format === "image"
+          ? { url: await ctx.storage.getUrl(post.body) }
+          : {}),
+      }))
+    );
+  },
+});
+
+
+
+import { mutation } from "./_generated/server";
+
+export const generateUploadUrl = mutation(async (ctx) => {
+  return await ctx.storage.generateUploadUrl();
+});
+
+export const createPost = mutation({
+  args: { 
+    storageId: v.string(), 
+    author: v.string(), 
+    location: v.optional(v.string()), 
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("posts", {
+      body: args.storageId,
+      author: args.author,
+      location: args.location, 
+      format: "image",
+    });
+  },
+});
+
+// export const getAllPostsByUser = query({
+
+// })
+
+// export const getAllPostsByLocation = query({
+
+// })
+
--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,13 +1,83 @@
-import React from 'react'
+'use client'
+
+import React, { useState, useRef, FormEvent } from 'react'
+import { useUser } from '@clerk/clerk-react'
 import Link from 'next/link'
-import { BsArrowRight } from 'react-icons/bs'
+
+import { api } from '../../convex/_generated/api';
+import { useMutation, useQuery } from 'convex/react';
 
 const Gallery = () => {
+
+  const posts = useQuery(api.images.getPosts) ?? [];
+
+  const { user } = useUser();
+  const name = user?.fullName as string;
+
+  const generateUploadUrl = useMutation(api.images.generateUploadUrl);
+  const uploadImage = useMutation(api.images.createPost);
+
+  const imageInput = useRef<HTMLInputElement>(null);
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+  async function handleSendImage(event: FormEvent) {
+    event.preventDefault();
+
+    const postUrl = await generateUploadUrl();
+    const result = await fetch(postUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': selectedImage!.type },
+      body: selectedImage,
+    });
+    const json = await result.json();
+    if (!result.ok) {
+      throw new Error(`Upload failed: ${JSON.stringify(json)}`);
+    }
+    const { storageId } = json;
+    await uploadImage({ storageId, author: name });
+
+    setSelectedImage(null);
+    imageInput.current!.value = '';
+  }
+
+
+
   return (
-    <div className="mt-8 flex flex-col">
-        gallery
+    <div className="flex flex-col">
+      gallery
+
+
+      <form onSubmit={handleSendImage}>
+        <input
+          type="file"
+          accept="image/*"
+          ref={imageInput}
+          onChange={(event) => setSelectedImage(event.target.files![0])}
+          className=""
+          disabled={selectedImage !== null}
+        />
+        <input
+          type="submit"
+          value="Upload"
+          disabled={selectedImage === null}
+        />
+      </form>
+
+      <ul className="columns-3 space-y-4">
+        {posts.map((post) => (
+          <li key={post._id}>
+            {post.format === "image" && (
+              <Image post={post} />
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   )
+}
+
+function Image({ post }: { post: { url: string } }) {
+  return <img src={post.url} height="full" width="full" className="object-cover w-fit h-auto" />;
 }
 
 export default Gallery

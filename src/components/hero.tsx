0cba8004ae73eb276d1f462671ddfb80a74c9bd8
Kane <kanelariviere@outlook.com>
2023-11-14 20:43:54 -0500

    style: resize for mobile

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
       <div className="flex flex-col -space-y-4 md:-space-y-2 font-migra mt-16">
         <Reveal>
           <div className="flex items-center justify-center p-2 tracking-widest font-bold">
-            <p className="text-[2.5rem] md:text-6xl lg:text-7xl xl:text-8xl">
+            <p className="text-[2rem] md:text-6xl lg:text-7xl xl:text-8xl">
               All
             </p>
             <div className="inline-flex items-center px-2 md:px-4 lg:px-6 mb-2 md:mb-4 -mr-2">
@@ -41,7 +41,7 @@ const Hero = () => {
                 className="mr-2 object-center object-cover h-8 w-8 md:h-14 md:w-14 lg:h-[4.5rem] lg:w-[4.5rem] border-2 md:border-4 border-white rounded-2xl"
               />
             </div>
-            <p className="text-[2.5rem] md:text-6xl lg:text-7xl xl:text-8xl">
+            <p className="text-[2rem] md:text-6xl lg:text-7xl xl:text-8xl">
               the proof
             </p>
           </div>
@@ -49,7 +49,7 @@ const Hero = () => {
 
         <Reveal>
           <div className="flex flex-col items-center justify-center font-bold p-4 tracking-widest">
-            <p className="text-[2.5rem] md:text-6xl lg:text-7xl xl:text-8xl mt-2">
+            <p className="text-[2rem] md:text-6xl lg:text-7xl xl:text-8xl mt-2">
               you need &nbsp;➺&nbsp; right
             </p>
           </div>
@@ -57,7 +57,7 @@ const Hero = () => {
 
         <Reveal>
           <div className="flex flex-col items-center justify-center font-bold p-4 tracking-widest">
-            <p className="flex items-center text-[2.5rem] md:text-6xl lg:text-7xl xl:text-8xl">
+            <p className="flex items-center text-[2rem] md:text-6xl lg:text-7xl xl:text-8xl">
               where you need it
               <TfiFaceSmile className="ml-2 md:ml-4 mb-2 text-2xl md:text-3xl lg:text-4xl xl:text-6xl" />
             </p>
